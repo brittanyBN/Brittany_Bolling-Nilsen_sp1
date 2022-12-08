3322061a31ab58a7b4c29b0bf79a1023c1965809
Brittany <brittany.bolling@aol.com>
2022-12-08 11:44:23 +0100

    Top table complete but missing validation,

--- wdt_app.js
+++ wdt_app.js
@@ -19,19 +19,37 @@ class StaffMember extends Employee {
     super(name, surname);
     this.picture = picture;
     this.email = email;
-    this.status = status;
-    this.outTime = outTime;
-    this.duration = duration;
-    this.expectedReturnTime = expectedReturnTime;
+    this.status = "";
+    this.outTime = "";
+    this.duration = "";
+    this.expectedReturnTime = "";
   }
   staffMemberIsLate() {
     return `${this.name} is late. Show a toast`;
   }
 }
 
-$("document").ready(function staffUserGet() {
-  const staffMembers = [];
+class DeliveryDriver extends Employee {
+  constructor(vehicle, name, surname, telephone, deliverAddress, returnTime) {
+    super(name, surname);
+    this.vehicle = vehicle;
+    this.telephone = telephone;
+    this.deliverAddress = deliverAddress;
+    this.returnTime = returnTime;
+  }
+  deliveryDriverIsLate() {
+    return `${this.name} is late. Show a toast`;
+  }
+  clearDriver() {
+    return "clear the selected row";
+  }
+}
 
+const staffMembers = [];
+const deliveryDriver = [];
+const status = [];
+
+function staffUserGet() {
   for (let i = 0; i < 5; i++) {
     $.ajax({
       url: "https://randomuser.me/api/",
@@ -56,7 +74,7 @@ $("document").ready(function staffUserGet() {
             "</td><td>" +
             `${email}` +
             "</td>" +
-            "<td id='status'></td>" +
+            "<td id='status'>--</td>" +
             "<td id='outTime'></td>" +
             "<td id='duration'></td>" +
             "<td id='returnTime'></td>" +
@@ -65,151 +83,206 @@ $("document").ready(function staffUserGet() {
       },
     });
   }
-});
-
-$("td").click(function () {
-  $(".selected").removeClass("selected");
-  $(this).addClass("selected");
-});
-
-// $(document).ready(function () {
-//   $("#staffTable tr").click(function () {
-//     $(this).addClass("selected").siblings().removeClass("selected");
-//     var value = $(this).find("td:first").html();
-//     alert(value);
-//   });
-//   $(".staffOut").on("click", function (e) {
-//     alert($("staffTable tr.selected td:first").html());
-//   });
-// });
-
-// $(document).ready(function () {
-//   $("#staffTable tr td").click(function () {
-//     $("#staffTable tr td").removeClass("selected");
-//     $(this).addClass("selected");
-//   });
-// });
-
-// $("#staffTable tr").click(function () {
-//   $(this).addClass("selected").siblings().removeClass("selected");
-//   var value = $(this).find("td:first").html();
-//   alert(value);
-// });
-
-// $(".staffOut").on("click", function (e) {
-//   alert($("#staffTable tr.selected td:first").html());
-// });
-
-// $(".staffOut").on("click", function (e) {
-//   alert($("#staffTable tr.selected td:first").html());
-// });
-
-// let selectedUser;
-
-// function toggleUser(i) {}
-
-// Select staff member from row
-// $(document).ready(function () {
-//   $("tableSelector").on("#staffUpdate", "click", function () {
-//     if (document.getElementsByClassName(".staffOut").clicked == true) {
-//       prompt("What time will staff member return?");
-//     }
-//   });
-// });
-
-//Click in or out button
-
-//Update Staff info in table
-
-// $(function () {
-//   $("##staffRow").click(function () {
-//     $("#staffUpdate").hover(highlight, highlight);
-//   });
-// });
-// function staffOut() {
-//   var userAdjective = prompt("Please provide an Adjective");
-//   alert(userAdjective);
-// }
-// const inputs = document.getElementById("deliveryInput").elements;
-
-class DeliveryDriver extends Employee {
-  constructor(name, surname, vehicle, telephone, deliverAddress, returnTime) {
-    super(name, surname);
-    this.vehicle = vehicle;
-    this.telephone = telephone;
-    this.deliverAddress = deliverAddress;
-    this.returnTime = returnTime;
-  }
-  deliveryDriverIsLate() {
-    return `${this.name} is late. Show a toast`;
-  }
 }
 
-const deliveryDriver = [];
+function clock(type) {
+  const date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let day = date.getDay();
+  let month = date.getMonth();
+  let year = date.getFullYear();
 
-function deliveryInput() {
-  const vehicle = document.getElementById("vehicle").value;
-  const name = document.getElementById("name").value;
-  const surname = document.getElementById("surname").value;
-  const telephone = document.getElementById("telephone").value;
-  const deliveryAddress = document.getElementById("address").value;
-  const returnTime = document.getElementById("returnTime").value;
-  const regPhone = /^\d{10}$/;
-  const regName = /\d+$/g;
-  const newDeliveryDriver = new DeliveryDriver(
-    vehicle,
-    surname,
-    name,
-    telephone,
-    deliveryAddress,
-    returnTime
-  );
-  deliveryDriver.push(newDeliveryDriver);
-  console.log(newDeliveryDriver);
-
-  if (vehicle == "" || !vehicle == "motorcycle" || !vehicle == "car") {
-    alert('Please enter "motorcyle" or "car".');
-    return false;
+  if (hours < 10) {
+    hours = "0" + hours;
   }
 
-  if (name == "" || regName.test(name)) {
-    alert("Please enter your name properly.");
-    return false;
+  if (minutes < 10) {
+    minutes = "0" + minutes;
   }
 
-  if (surname == "" || regName.test(surname)) {
-    alert("Please enter your surname properly.");
-    return false;
+  if (seconds < 10) {
+    seconds = "0" + seconds;
   }
 
-  if (telephone == "" || !regPhone.test(telephone)) {
-    alert("Please enter valid phone number.");
-    return false;
+  if (day < 10) {
+    day = "0" + day;
   }
 
-  if (deliveryAddress == "") {
-    alert("Please enter your address.");
-    return false;
+  if (month < 10) {
+    month = "0" + month;
   }
 
-  if (returnTime == "") {
-    alert("Please enter a valid return time.");
-    return false;
+  if (type === "currentTime") {
+    return hours + ":" + minutes;
   }
 
-  $("#deliveryT").append(
-    "<tr><td>" +
-      `${vehicle}` +
-      "</td><td>" +
-      `${name}` +
-      "</td><td>" +
-      `${surname}` +
-      "</td><td>" +
-      `${telephone}` +
-      "</td><td>" +
-      `${deliveryAddress}` +
-      "</td><td>" +
-      `${returnTime}` +
-      "</td></tr>"
-  );
+  if (type === "time") {
+    return (
+      day +
+      "/" +
+      month +
+      "/" +
+      year +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      " " +
+      seconds
+    );
+  }
+  return;
 }
+
+$("document").ready(function () {
+  staffUserGet();
+
+  setInterval(function () {
+    $("#date").text(clock("time"));
+  }, 1000);
+
+  $("#staffTable tbody").on("click", "tr", function () {
+    $(this).toggleClass("selected");
+  });
+
+  $("#staffOut").click(function () {
+    // Identify correct staff member by matching their unique email to the selected user.
+    $("#staffTable .selected").each(function () {
+      let email = $(this).find("td:eq(3)").text();
+      const staffMember = staffMembers.find(
+        (staffMember) => staffMember.email === email
+      );
+      console.log(staffMember);
+
+      //Update status in table and in object
+      let status = $(this).find("td:eq(4)").text();
+      staffMember.status = "Out";
+      console.log(status);
+      $(this).find("td:eq(4)").text("Out");
+
+      //Prompt receptionist for return time, update table with answer and update object
+      let leaveT = prompt("What time will the staff member leave? hh:mm");
+
+      //If statement to validate time
+
+      let leaveTime = $(this).find("td:eq(5)").text(leaveT);
+      staffMember.leaveTime = leaveT;
+      console.log(leaveTime);
+      $(this).find("td:eq(5)").text(leaveT);
+
+      //Prompt receptionist for return time, update table with answer and update object
+      let returnT = prompt("What time will the staff member return? hh:mm");
+
+      //If statement to validate time
+
+      let returnTime = $(this).find("td:eq(7)").text(returnT);
+      staffMember.returnTime = returnT;
+      console.log(returnTime);
+      $(this).find("td:eq(7)").text(returnT);
+
+      //Prompt receptionist for out time, update table with answer and update object
+      // let outT = prompt("What time did the staff member leave? hh:mm");
+      // let outTime = $(this).find("td:eq(5)").text(outT);
+      // staffMember.outTime = outT;
+      // console.log(outTime);
+      // $(this).find("td:eq(5)").text(outT);
+
+      //Calculate duration of time staff member is out
+      let arrayReturnT = returnT.split(":");
+      console.log(arrayReturnT);
+      let arrayLeaveT = leaveT.split(":");
+      console.log(arrayLeaveT);
+      let hours = arrayReturnT[0] - arrayLeaveT[0];
+      console.log(hours);
+      let minutes = arrayReturnT[1] - arrayLeaveT[1];
+      console.log(minutes);
+      let durationT = hours + " hours and " + minutes + " minutes";
+      console.log(durationT);
+      let durationTime = $(this).find("td:eq(6)").text(durationT);
+      staffMember.durationTime = durationT;
+      console.log(durationTime);
+      $(this).find("td:eq(6)").text(durationT);
+    });
+  });
+
+  $("#addDelivery").click(function () {
+    let vehicle = $("#vehicle").val();
+    const name = $("#name").val();
+    const surname = $("#surname").val();
+    const telephone = $("#telephone").val();
+    const deliveryAddress = $("#address").val();
+    const returnTime = $("#returnTimeNow").val();
+    const regPhone = /^\d{10}$/;
+    const regName = /\d+$/g;
+
+    let error = 0;
+
+    if (vehicle == "" || !vehicle == "motorcycle" || !vehicle == "car") {
+      alert('Please enter "motorcyle" or "car".');
+    } else if (vehicle === "car") {
+      var cell = $("#vehicle");
+      cell.text('<i class="bi bi-car-front"></i>');
+    }
+
+    if (name == "" || regName.test(name)) {
+      alert("Please enter your name properly.");
+      error++;
+    }
+
+    if (surname == "" || regName.test(surname)) {
+      alert("Please enter your surname properly.");
+      error++;
+    }
+
+    if (telephone == "" || !regPhone.test(telephone)) {
+      alert("Please enter valid phone number.");
+      error++;
+    }
+
+    if (deliveryAddress == "") {
+      alert("Please enter your address.");
+      error++;
+    }
+
+    if (returnTime == "") {
+      alert("Please enter a valid return time.");
+      error++;
+    }
+
+    console.log(error);
+
+    if (error === 0) {
+      const newDeliveryDriver = new DeliveryDriver(
+        vehicle,
+        surname,
+        name,
+        telephone,
+        deliveryAddress,
+        returnTime
+      );
+      deliveryDriver.push(newDeliveryDriver);
+      console.log(newDeliveryDriver);
+
+      $("#deliveryT").append(
+        "<tr><td>" +
+          `${vehicle}` +
+          "</td><td>" +
+          `${name}` +
+          "</td><td>" +
+          `${surname}` +
+          "</td><td>" +
+          `${telephone}` +
+          "</td><td>" +
+          `${deliveryAddress}` +
+          "</td><td>" +
+          `${returnTime}` +
+          "</td></tr>"
+      );
+      $("#deliveryTable tbody").on("click", "tr", function () {
+        $(this).toggleClass("selected");
+      });
+    }
+  });
+});
